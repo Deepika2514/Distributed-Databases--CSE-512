af5da945fac8b6f3884d07bb78043caaa15d2e26
Deepika Patra <46588612+Deepika2514@users.noreply.github.com>
2020-01-29 10:59:37 -0700

    Assignment 2

---
+++ Assignment2_Interface.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.7
+#
+# Assignment2 Interface
+#
+
+import psycopg2
+import os
+import sys
+# Donot close the connection inside this file i.e. do not perform openconnection.close()
+def RangeQuery(ratingMinValue, ratingMaxValue, openconnection, outputPath):
+    #Implement RangeQuery Here.
+    pass #Remove this once you are done with implementation
+
+def PointQuery(ratingValue, openconnection, outputPath):
+    #Implement PointQuery Here.
+    pass # Remove this once you are done with implementation
